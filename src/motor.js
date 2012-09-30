7dc2cac897880883c7cff5b50a799e35bb0f4d1b
Craig Campbell <iamcraigcampbell@gmail.com>
2012-09-30 13:27:57 -0400

    Initial checkin

---
+++ src/motor.js
@@ -0,0 +1,97 @@
+VERSION = '0.1';
+fs = require('fs');
+path = require('path');
+razor = require('./razor.js');
+output = '';
+
+Motor = (function() {
+    function _wrap(fn, name) {
+        var code = 'Razor.Templates[\'' + name + '\'] = function(args) {\n';
+        code += '    ' + fn.replace(/\n/g, '\n    ') + '\n};\n';
+        return code;
+    }
+
+    function _startOutput() {
+        var str = 'Razor = Razor || {};\n';
+        str += 'Razor.Templates = {};\n';
+        str += 'Razor.render = function(name, args) {\n';
+        str += '    if (Razor.Templates[name]) {\n';
+        str += '        return Razor.Templates[name].call(Razor, args);\n';
+        str += '    }\n';
+        str += '    return \'\';\n';
+        str += '};\n\n';
+
+        return str;
+    }
+
+    function _writeToDisk(dest) {
+        fs.writeFileSync(dest, output.replace(/ +(?=\n)/g, ''), 'UTF-8');
+        output = '';
+    }
+
+    return {
+        processFile: function(src, dest, append) {
+            if (!output) {
+                output = _startOutput();
+            }
+
+            var contents = fs.readFileSync(src, 'UTF-8'),
+                fn = razor.generate(contents),
+                name = src.split('/').pop().split('.')[0];
+
+            output += _wrap(fn, name);
+
+            if (!append) {
+                // finish
+            }
+        },
+
+        processDirectory: function(src, dest) {
+            fs.readdir(src, function(err, files) {
+                files.forEach(function(file) {
+                    var path = src + '/' + file;
+                    if (fs.statSync(path).isDirectory()) {
+
+                        // ignore subdirectories
+                        return;
+                    }
+
+                    Motor.processFile(path, dest, true);
+                });
+
+                _writeToDisk(dest);
+            });
+        }
+    };
+}) ();
+
+exports.start = function(args) {
+    args = args.slice(2);
+
+    if (args.length === 0) {
+        console.log('need to specify file or directory');
+        return;
+    }
+
+    if (!path.existsSync(args[0])) {
+        console.log(args[0] + ' is not a file or directory');
+        return;
+    }
+
+    if (fs.statSync(args[0]).isDirectory()) {
+        if (!args[1]) {
+            console.log('need to specify an output path for compiled templates');
+            return;
+        }
+
+        Motor.processDirectory(args[0].replace(/\/+$/g, ''), args[1]);
+        return;
+    }
+
+    if (args[0].split('.').pop() != 'rzml') {
+        console.log('file extension should be .rzml');
+        return;
+    }
+
+    Motor.processFile(args[0], args[1] || args[0].replace('.rzml', '.js'));
+};
---
+++ src/razor.js
@@ -0,0 +1,96 @@
+
+Razor = (function() {
+    function _indent(count) {
+        count *= 4;
+        return new Array(++count).join(' ');
+    }
+
+    function _templateToJavascript(string) {
+
+        // replace regular variables
+        string = string.replace(new RegExp(Razor.start + '=\\s*(.*?)\\s*' + Razor.end, 'g'), "_QUOTE_ + $1 + _QUOTE_").replace(/@/g, 'args.');
+
+        var regex = new RegExp(Razor.start + '\\s*(.*?):?\\s*' + Razor.end, 'g'),
+            bits = string.split(regex),
+            length = bits.length,
+            bit,
+            start,
+            line_ending,
+            code = [],
+            first_word,
+            expression,
+            indent = 0,
+            i;
+
+        for (i = 0; i < length; i++) {
+            bit = bits[i].replace(/^\s+/, '').replace(/\s+$/, '');
+
+            if (!bit) {
+                continue;
+            }
+
+            if (i % 2) {
+                first_word = bit.split(' ')[0];
+                bit = bit.replace(first_word, '');
+                line_ending = ';';
+
+                switch (first_word) {
+                    case 'case':
+                        line_ending = ':';
+                        break;
+                    case 'elseif':
+                        first_word = 'else if';
+                    case 'if':
+                    case 'else':
+                    case 'for':
+                    case 'switch':
+                        line_ending = ' {';
+                        break;
+                }
+
+                if (first_word.indexOf('end') === 0) {
+                    indent -= 1;
+                    code.push(_indent(indent) + '}\n\n');
+                    continue;
+                }
+
+                expression = line_ending == ';';
+
+                if (expression && bit.charAt(bit.length - 1) == ';') {
+                    line_ending = '';
+                }
+
+                code.push(_indent(indent) + first_word + bit.replace(/@/g, 'args.') + line_ending + '\n');
+                if (!expression) {
+                    indent += 1;
+                }
+                if (first_word == 'break') {
+                    indent -= 1;
+                }
+                continue;
+            }
+
+            start = !start ? 'var t = ' : 't += ';
+            code.push(_indent(indent) + start + '\'' + bit.replace(/\'/g, "\\'").replace(/\n/g, '').replace(/_QUOTE_/g, "'") + '\'' + ';\n');
+        }
+        code.push('return t;');
+console.log(code);
+
+        return code.join('');
+    }
+
+    return {
+        start: '\\<\\?',
+        end: '\\?\\>',
+
+        generate: function(string) {
+            return _templateToJavascript(string);
+        },
+
+        compile: function(string) {
+            return new Function(string);
+        }
+    };
+}) ();
+
+exports.generate = Razor.generate;
